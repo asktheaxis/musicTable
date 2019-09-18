b14b2897c681039dafd36e3bb0bdce2109eb4098
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2019-09-18 12:18:26 -0400

    Merge branch 'master' into User_Auth
    # Conflicts:
    #	app/src/main/java/com/example/alspicks/MainActivity.java

--- app/src/main/java/com/example/alspicks/MainActivity.java
+++ app/src/main/java/com/example/alspicks/MainActivity.java
@@ -26,9 +26,7 @@ import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    int x;
-    double y;
-    boolean a;
+
     private EditText edtArtist, edtAlbum, edtYear, edtStyle;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
