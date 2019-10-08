4a8b3778973238a542308220711841a519ee396c
asktheaxis <37046448+asktheaxis@users.noreply.github.com>
2019-10-08 17:57:20 -0400

    Moved sign in and sign out buttons to a new "account" fragment
    slight modifications to the new tunes table format

--- app/src/main/java/com/example/alspicks/MainActivity.java
+++ app/src/main/java/com/example/alspicks/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_light);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
