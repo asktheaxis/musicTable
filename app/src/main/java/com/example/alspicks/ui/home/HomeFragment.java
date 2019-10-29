688c5ea1c17bd9ecee649eb1f18e68e3094f64f1
asktheaxis <37046448+asktheaxis@users.noreply.github.com>
2019-10-29 19:06:54 -0400

    Discogs API
    search works as follows:
    if user only types in an artist it searches matching artists //POTENTIALLY USELESS FOR OUR APP: CHANGE TO SHOW ALBUMS BY ENTERED BAND
    if they only enter an album it searches for matching albums
    if they enter artist and album it searches for albums by that artist

--- app/src/main/java/com/example/alspicks/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/alspicks/ui/home/HomeFragment.java
@@ -39,8 +39,6 @@ import org.json.JSONObject;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 import static com.firebase.ui.auth.AuthUI.getApplicationContext;
@@ -52,7 +50,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private Spinner edtStyle;
     private ImageView imageView1, imageView2, imageView3;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private String albumNameEncoded, artistNameEncoded;
+    private String albumYear, artistFinal, albumFinal;
+    private ArrayList<String> albumStyles = new ArrayList<>();
     private RequestQueue requestQueue;
 
     private static final String TAG = "MainActivity";
@@ -77,6 +76,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         edtAlbum = root.findViewById(R.id.edtAlbum);
         edtYear = root.findViewById(R.id.edtYear);
         edtStyle = root.findViewById(R.id.edtStyle);
+
+        //for testing
         imageView1 = root.findViewById(R.id.imageView3);
         imageView2 = root.findViewById(R.id.imageView4);
         imageView3 = root.findViewById(R.id.imageView5);
@@ -120,28 +121,38 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         startActivity(intent);
     }
 
-
-
     private void addAlbum() {
 
         SharedViewModel sharedViewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(SharedViewModel.class);
-        final String albumArtist = edtArtist.getText().toString();
-        final String albumName = edtAlbum.getText().toString();
-        final String albumYear = edtYear.getText().toString();
-        final String albumStyle = edtStyle.getSelectedItem().toString();
+        final String artistInput = edtArtist.getText().toString();
+        final String albumInput = edtAlbum.getText().toString();
         final String userID = sharedViewModel.getUid();
         final String albumPath;
 
-        searchArtist(albumArtist, albumName);
+        String searchType = null;
+        String query = null;
+        if (!artistInput.equals("") && !albumInput.equals("")){
+            searchType = "title";
+            query = artistInput + " - " + albumInput;
+        } else if (artistInput.equals("") && !albumInput.equals("")) {
+            searchType = "album";
+            query = albumInput;
+        } else {
+            searchType = "artist";
+            query = artistInput;
+        }
+
+        search(searchType, query);
 
 
-        // Create a new album with Artist, Album, Year, and Genre
+
+        /*// Create a new album with Artist, Album, Year, and Genre
         final Map<String, Object> album = new HashMap<>();
         album.put("artist", albumArtist);
         album.put("name", albumName);
         album.put("year", albumYear);
         album.put("style", albumStyle);
-        album.put("origUser", sharedViewModel.getUid());
+        album.put("origUser", sharedViewModel.getUid());*/
 
 
         // Add a new document with a generated ID
@@ -159,73 +170,24 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
     }
 
-
-    /*private String createAlbumSearchURL(String name){
+    private String createSearchURL(String search, String query){
+        String searchEncoded = null;
+        String queryEncoded = null;
         try {
-            albumNameEncoded = URLEncoder.encode(name, "UTF-8");
-        } catch (UnsupportedEncodingException e){
-            Log.w(TAG, "Error encoding URL", e);
-        }
-        String url = "https://api.discogs.com/database/search?type=release_title&q=" + albumNameEncoded + "&key=" + BuildConfig.CONSUMER_KEY + "&secret=" + BuildConfig.CONSUMER_SECRET;
-        return url;
-    }*/
-
-    private String createArtistSearchURL(String artist, String album){
-        try {
-            artistNameEncoded = URLEncoder.encode(artist, "UTF-8");
-            albumNameEncoded = URLEncoder.encode(album, "UTF-8");
+            searchEncoded = URLEncoder.encode(search, "UTF-8");
+            queryEncoded = URLEncoder.encode(query, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             Log.w(TAG, "Error encoding URL", e);
         }
-        //FIXME https://api.discogs.com/database/search?type=album&q=clear%20spot&key=JmAzdrpGIbiMhPzTgmQc&secret=ucIcmmBonEOxgCqmxlwsEPfRfArHigfK
-        String url = "https://api.discogs.com/database/search?q={?title=" + artistNameEncoded + " - " + albumNameEncoded + "}&key=" + BuildConfig.CONSUMER_KEY + "&secret=" + BuildConfig.CONSUMER_SECRET;
+        String url = "https://api.discogs.com/database/search?type=" + searchEncoded + "&q=" + queryEncoded + "&key=" + BuildConfig.CONSUMER_KEY + "&secret=" + BuildConfig.CONSUMER_SECRET;
         return url;
     }
 
-    /*private void buildRequest(String albumName){
+    private void search(String searchType, String query){
         RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
         JsonObjectRequest objectRequest = new JsonObjectRequest(
                 Request.Method.GET,
-                createAlbumSearchURL(albumName),
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response){
-                        //Convert Json to Album Object to add to DB
-                        Log.w("Rest Response", response.toString());
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("results");
-                            JSONObject jsonObject = jsonArray.getJSONObject(0);
-                            Log.w("The first result from Discogs is ", jsonObject.toString());
-                            String title = jsonObject.getString("title");
-                            Log.w("It's title is ", title);
-                            JSONArray styles = jsonObject.getJSONArray("style");
-                            Log.w("It's style(s) are ", styles.getString(0) + ", ");
-                            String year = jsonObject.getString("year");
-                            Log.w("Year of release ", year);
-                            String url = jsonObject.getString("thumb");
-                            //fetchAlbumImages(url);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.w("Error requesting Json data", error.toString());
-                    }
-                }
-        );
-
-        requestQueue.add(objectRequest);
-    }*/
-
-    private void searchArtist(String artist, String album){
-        RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                createArtistSearchURL(artist, album),
+                createSearchURL(searchType, query),
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
@@ -235,23 +197,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
                         try {
                             JSONArray jsonArray = response.getJSONArray("results");
                             JSONObject overallResultsObject = jsonArray.getJSONObject(0);
-                            overallResultsObject.get("year");
+                            //overallResultsObject.get("year");
                             ArrayList<String> urls = new ArrayList<>();
-                            final Map<String, Object> album = new HashMap<>();
-                            //album.put("artist", albumArtist);
-                            //album.put("name", albumName);
-                            //album.put("year", albumYear);
-                            //album.put("style", albumStyle);
-                            //album.put("origUser", sharedViewModel.getUid());
                             for (int i = 0; i < 4; i++) {
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                String url = jsonObject.getString("thumb");
+                                String url = jsonObject.getString("cover_image");
                                 urls.add(url);
                                 Log.w("Discogs result ", jsonObject.toString());
-                                JSONArray styles = jsonObject.getJSONArray("style");
-                                Log.w("It's style(s) are ", styles.getString(0) + ", ");
-                                String year = jsonObject.getString("year");
-                                Log.w("Year of release ", year);
                             }
                             Picasso.with(getContext()).load(urls.get(0)).fit().into(imageView1);
                             Picasso.with(getContext()).load(urls.get(1)).fit().into(imageView2);
@@ -271,85 +223,4 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
         requestQueue.add(objectRequest);
     }
-
-    /*private void searchAlbum(String albumName){
-        RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                createAlbumSearchURL(albumName),
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response){
-                        //Convert Json to Album Object to add to DB
-                        Log.w("Rest Response", response.toString());
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("results");
-                            ArrayList<String> urls = new ArrayList<>();
-                            for (int i = 0; i < 4; i++) {
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                String url = jsonObject.getString("thumb");
-                                urls.add(url);
-                            }
-
-                            Picasso.with(getContext()).load(urls.get(0)).fit().into(imageView1);
-                            Picasso.with(getContext()).load(urls.get(1)).fit().into(imageView2);
-                            Picasso.with(getContext()).load(urls.get(2)).fit().into(imageView3);
-                            /*Log.w("The first result from Discogs is ", jsonObject.toString());
-                            String title = jsonObject.getString("title");
-                            Log.w("It's title is ", title);
-                            JSONArray styles = jsonObject.getJSONArray("style");
-                            Log.w("It's style(s) are ", styles.getString(0) + ", ");
-                            String year = jsonObject.getString("year");
-                            Log.w("Year of release ", year);
-                            String url = jsonObject.getString("thumb");
-                            fetchAlbumImages(url);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.w("Error requesting Json data", error.toString());
-                    }
-                }
-        );
-
-        requestQueue.add(objectRequest);
-    }*/
-
-    /*private void fetchAlbumImages(String url) {
-        Picasso.with(getContext()).load(url).fit().into(imageView);
-    }*/
-
-    /*public void buildRequest(String albumName) {
-
-        RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
-        StringRequest stringRequest = new StringRequest(createAlbumSearchURL(albumName), new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONArray jsonArray = jsonObject.getJSONArray();
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jo = jsonArray.getJSONObject(i);
-                        // Do you fancy stuff
-                        // Example: String gifUrl = jo.getString("url");
-                        Log.w("Successfully returned JSON", response);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.w("Volley JSON error", error);
-            }
-        });
-        requestQueue.add(stringRequest);
-    }*/
-
 }
