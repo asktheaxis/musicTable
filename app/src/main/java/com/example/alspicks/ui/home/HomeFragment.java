3da619f96b6e8990db32f936704705b28d118951
asktheaxis <37046448+asktheaxis@users.noreply.github.com>
2019-11-04 12:51:54 -0500

    black box tests/implemenation

--- app/src/main/java/com/example/alspicks/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/alspicks/ui/home/HomeFragment.java
@@ -50,6 +50,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private String albumYear, artistFinal, albumFinal;
     private ArrayList<String> albumStyles = new ArrayList<>();
+    ArrayList<String> urls = new ArrayList<>();
     private int artistId = 0;
 
     private static final String TAG = "MainActivity";
@@ -137,6 +138,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             searchType = "album";
             query = albumInput;
             searchAlbums(searchType, query);
+            setImages(urls);
         } else {
             searchType = "artist";
             query = artistInput;
@@ -183,20 +185,23 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         return url;
     }
 
-    private String createArtistSearchURL(int id){
-        String artistId = Integer.toString(id);
-        String idEncoded = null;
-        try {
-            idEncoded = URLEncoder.encode(artistId, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            Log.w(TAG, "Error encoding URL", e);
+    public String createArtistSearchURL(int id) throws Exception {
+        if (id < 0){
+            throw new Exception("artist id must be greater than zero!");
+        } else {
+            String artistId = Integer.toString(id);
+            String idEncoded = null;
+            try {
+                idEncoded = URLEncoder.encode(artistId, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                Log.w(TAG, "Error encoding URL", e);
+            }
+            String url = "https://api.discogs.com/artists/" + idEncoded + "/releases?sort=year&sort_order=asc&key=" + BuildConfig.CONSUMER_KEY + "&secret=" + BuildConfig.CONSUMER_SECRET;
+            return url;
         }
-        String url = "https://api.discogs.com/artists/" + idEncoded + "/releases?sort=year&sort_order=asc&key=" + BuildConfig.CONSUMER_KEY + "&secret=" + BuildConfig.CONSUMER_SECRET;
-        return url;
     }
 
-    //FIXME MAKE PRIVATE AFTER MONDAY BLACK BOX
-    public void searchAlbums(String searchType, String query){
+    /*private void searchAlbums(String searchType, String query){
         RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
         JsonObjectRequest objectRequest = new JsonObjectRequest(
                 Request.Method.GET,
@@ -225,6 +230,42 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         );
 
         requestQueue.add(objectRequest);
+    }*/
+
+    //FIXME MAKE PRIVATE AFTER MONDAY BLACK BOX
+    public ArrayList<String> searchAlbums(String searchType, String query){
+        RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                createSearchURL(searchType, query),
+                null,
+                response -> {
+                    //Convert Json to Album Object to add to DB
+                    Log.w("Rest Response", response.toString());
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("results");
+                        for (int i = 0; i < 4; i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String url = jsonObject.getString("cover_image");
+                            urls.add(url);
+                            Log.w("Discogs Albums result ", jsonObject.toString());
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Log.w("Error requesting Json data", error.toString())
+        );
+        requestQueue.add(objectRequest);
+        return urls;
+    }
+
+    private void setImages(ArrayList<String> urls){
+        for(String link : urls) {
+            Picasso.with(getContext()).load(urls.get(0)).fit().into(imageView1);
+            Picasso.with(getContext()).load(urls.get(1)).fit().into(imageView2);
+            Picasso.with(getContext()).load(urls.get(2)).fit().into(imageView3);
+        }
     }
 
     private void searchArtist(String searchType, String query){
@@ -241,7 +282,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
                         artistId = overallResultsObject.getInt("id");
                         String albumsUrl = createArtistSearchURL(artistId);
                         searchAlbumByArtist(albumsUrl);
-                    } catch (JSONException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 },
--- app/src/test/java/com/example/alspicks/ui/home/HomeFragmentTest.java
+++ app/src/test/java/com/example/alspicks/ui/home/HomeFragmentTest.java
@@ -2,14 +2,6 @@ package com.example.alspicks.ui.home;
 
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -17,14 +9,21 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Objects;
 
 
 class HomeFragmentTest extends HomeFragment {
 
     @Test
-    void createSearchURL() throws IOException {
-        String url = createSearchURL("artist", "frank zappa");
+    void createArtistSearchURLTest() throws Exception {
+        try {
+            createArtistSearchURL(-1);
+            createArtistSearchURL(0);
+
+        } catch (Exception e){
+            Log.w("Artist id must be greater than zero!", e);
+        }
+
+        String url = createArtistSearchURL(92623);
         final URL urll = new URL(url);
         HttpURLConnection huc = (HttpURLConnection) urll.openConnection();
         huc.setRequestMethod("HEAD");
@@ -35,30 +34,8 @@ class HomeFragmentTest extends HomeFragment {
     }
 
     @Test
-    void searchAlbums() throws IOException {
-        //searchAlbums("album", "sleep dirt");
-        ArrayList<String> urls = new ArrayList<>();
-        RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                createSearchURL("album", "sleep dirt"),
-                null,
-                response -> {
-                    try {
-                        JSONArray jsonArray = response.getJSONArray("results");
-                        for (int i = 0; i < 4; i++) {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            String url = jsonObject.getString("cover_image");
-                            urls.add(url);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                },
-                error -> Log.w("Error requesting Json data", error.toString())
-        );
-        requestQueue.add(objectRequest);
-
+    void searchAlbumsTest() throws IOException {
+        ArrayList<String> urls = searchAlbums("album", "sleep dirt");
         for(String link : urls){
             final URL url = new URL(link);
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
